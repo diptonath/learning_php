652bf47678b71597153dd465a3b2692f62b8d4ed
Dipto Nath <45964738+diptonath@users.noreply.github.com>
2023-03-27 23:51:33 +0600

    update

--- ostad/module-8/assignment/auth_session.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION["fname"])) {
-        header("Location: login.php");
-        exit();
-    }
-?>
--- ostad/module-8/assignment/login.php
+++ ostad/module-8/assignment/login.php
@@ -1,66 +1,26 @@
 <?php
-require('db.php');
 session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-if (isset($_POST['submit'])) {
-
-    if (isset($_POST['useremail']) && isset($_POST['userpassword'])) {
-
-            // removes backslashes
-            $useremail = stripslashes($_REQUEST['useremail']);
-            $userpassword = stripslashes($_REQUEST['userpassword']);
-
-            // escapes special characters in a string
-            $useremail = mysqli_real_escape_string($con, $useremail);
-            $userpassword = mysqli_real_escape_string($con, $userpassword);
-            $hashedPassword = md5($userpassword);
-
-            $query = "SELECT `fname` FROM `users` WHERE `email` = '$useremail' AND `password` = '$hashedPassword'";
-            $result = mysqli_query($con, $query);
-
-            $row = mysqli_fetch_array($result);
-            $rows = mysqli_num_rows($result);
-
-            if ($rows == 1) {
-                $_SESSION['fname'] = $row['fname'];
-
-                // Redirect to user dashboard page
-                header("Location: welcome.php");
-            } else {
-                echo "<div class='form'>
-                    <h3>Incorrect Username/password.</h3><br/>
-                    <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
-                    </div>";
-            }
-
+    //Validation checks
+    if(empty($email) || empty($password)) {
+        echo "";
+        $_SESSION["allfild"] = "Please fill all the fields.";
+        header("Location: loginform.php");
+        exit();
     }
     else {
-        echo "Please fill out all the fields for login.\n";
-    }
+        if($_SESSION["email"] == $email && $_SESSION["password"] == $password){
+            $_SESSION["success"] = $_SESSION["fname"];
+            header("Location: welcome.php");
+            exit();
+        }else{
+            $_SESSION["invalid"] = "Invalid login credentials";
+            header("Location: loginform.php");
+            exit(); 
+        }
+        
+    };
 }
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles.css">
-    <title>Login Form</title>
-</head>
-<body>
-    <form action="" method="POST">
-        <div class="container">
-            <h1>Login</h1>
-
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="useremail" id="email" required>
-
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="userpassword" id="psw" required>
-            <hr>
-            <input type="submit" name="submit" value="Login">
-        </div>
-    </form>
-</body>
-</html>
