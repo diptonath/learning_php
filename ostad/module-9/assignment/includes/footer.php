13fcfe77ce371def3d494e04340473a59a4363b0
Dipto Nath <45964738+diptonath@users.noreply.github.com>
2023-04-03 22:37:06 +0600

    module 9

---
+++ ostad/module-9/assignment/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-gray-800 text-white py-4">
+    <div class="container mx-auto text-center">
+        <p>&copy; <?= date('Y'); ?> My Personal Blog. All rights reserved. | Powered By Dipto Nath</p>
+    </div>
+</footer>
+</body>
+
+</html>
